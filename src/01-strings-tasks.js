a08c7031d2cc138a0f7b3cbccc2d65fe0299c1e8
Gleb LAgutko <gleblagutko@gmail.com>
2020-12-03 12:51:47 +0300

    feat : done string and array

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -234,7 +234,12 @@ function getRectangleString(width, height) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
+function encodeToRot13(str) {
+  const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  const index = x => input.indexOf(x);
+  const translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
 
 }
 
